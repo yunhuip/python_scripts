3cc45906eb8485fb82e8a7e9136c0baef956b8ae
yunhuip <yunhuipengys@gmail.com>
2021-10-06 22:50:03 -0400

    Update reindex_pdb.py

--- reindex_pdb.py
+++ reindex_pdb.py
@@ -1,176 +1,126 @@
-#!/usr/bin/env python
-docstring='''
-reindex_pdb.py startindex infile.pdb outfile.pdb
-    Rearrange residue number of "infile.pdb" so the first residue start from 
-    "startindex". if the structure contains any missing residue, residue
-    number gaps will be removed.
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 
-reindex_pdb.py seq.fasta  infile.pdb outfile.pdb
-    Rearrange residue number of "infile.pdb" according to "seq.fasta" so the 
-    residue index is the same as that in "seq.fasta"
-
-options:
-    -clean={true,false} whether to remove lines other than "ATOM  " entries
-'''
-import sys,os
+This is a temporary script file.
+"""
+import os
+import numpy as np
+import pandas as pd 
+import requests
 import re
+import csv
 
-def reindex_pdb_by_index(startindex=1,PDBtxt=''):
-    '''
-    reindex residue number of PDB format text
-    
-    options:
-        startindex - index of first residue
-        PDBtxt     - text of input PDB to be reindexed
-    '''
-    PDBtxt_reindex=''
-    current_old_index='' # residue number in origin PDB
-    warn_chainID='' # warning about new chain ID
+## Retrive histone sequence by uniprotID and perform MSA using clustal-omega
+def uniprotID_to_fa_msa(uniprotID_file, fa_file):
+    uniprot_ID = pd.read_table(uniprotID_file,header = None)
+    with open(fa_file, "w") as fh:
+        for i in range(0, len(uniprot_ID)):    
+            response = requests.post("https://www.uniprot.org/uniprot/" + uniprot_ID.iloc[i][0] + ".fasta")
+            fh.write(response.text) 
+    os.system("./clustal-omega-1.2.3-macosx -i " + fa_file + " -o " + fa_file[0:-3] + "_msa.fa --auto -v --force")
 
-    for line in PDBtxt.splitlines():
-        if len(line)<27 or (not line.startswith("ATOM  ") and \
-            not line.startswith("HETATM") and not line.startswith("TER")):
-            PDBtxt_reindex+=line+'\n'
-            continue
-        elif not line[16] in ['A',' ']: # alternative location identifier
-            continue
-        resSeq=line[22:27] # residue sequence number
-        current_chainID=line[21] # chain identifier
+def joint_list_to_string(input_list,sting_seq):       
+    # Converting integer list to string list 
+    s = [str(i) for i in input_list]       
+    # Join list items using join() 
+    res = str(sting_seq.join(s)) 
+    return(res)
 
-        if not current_old_index: # first residue encountered
-            current_old_index=resSeq # residue number in origin PDB
-            current_new_index=int(startindex)
-            chainID=current_chainID
-            resSeq_new=str(current_new_index)
-            resSeq_new=' '*(4-len(resSeq_new))+resSeq_new+' '
-        elif current_chainID!=chainID:
-            if warn_chainID!=current_chainID:
-                sys.stderr.write(
-                    "Warning! Discarding chain '%s'\n"%current_chainID)
-                warn_chainID=current_chainID
-            continue
-        elif resSeq!=current_old_index:
-            current_new_index+=1
-            current_old_index=resSeq
-            resSeq_new=str(current_new_index)
-            resSeq_new=' '*(4-len(resSeq_new))+resSeq_new+' '
-        PDBtxt_reindex+=line[:16]+' '+line[17:22]+resSeq_new+line[27:]+'\n'
-    return PDBtxt_reindex
-
-def find_next_amino_acid(sequence_ref,sequence_pdb,startindex=0):
-    '''find the first instance of amino acid whose index is greater than
-    startindex. index is one-based. return -1 if not found. return negative
-    if the position is gap in sequence_ref
-    '''
-    startindex=abs(startindex)
-    for resi in range(startindex,len(sequence_pdb)):
-        if sequence_pdb[resi]!='-':
-            return (resi+1)*(-1)**(sequence_ref[resi]=='-')
-    return 0
-
-def reindex_pdb_by_sequence(sequence_ref,PDBtxt):
-    '''NWalign "sequence_pdb" to "sequence_ref" and renumber residue index
-    according to sequence_ref.
-    
-    unaligned residues in sequence_pdb will be removed.
-    only ATOM or MSE will be parsed
-    '''
-    #### convert PDB to fasta ####
-    from pdb2fasta import pdbtxt2seq
-    header_list,sequence_list=pdbtxt2seq(PDBtxt,PERMISSIVE="MSE",allowX=True)
-    sequence_pdb=sequence_list[0]
-
-    #### perform NeedlemanWunsch ####
-    from NWalign import calcualte_score_gotoh,trace_back_gotoh
-    # idir (DP path); jpV (Horizontal jump number); jpH (Vertical jump number)
-    idir,jpV,jpH=calcualte_score_gotoh(sequence_ref,sequence_pdb)
-    # sequenceA (aligned f1); sequenceB (aligned f2)
-    sequence_ref,sequence_pdb=trace_back_gotoh(idir,jpV,jpH,
-        sequence_ref,sequence_pdb)
-
-    #### reindex PDB text ####
-    PDBtxt_reindex=''
-    current_old_index='' # residue number in origin PDB
-    warn_chainID='' # warning about new chain ID
-
-    for line in PDBtxt.splitlines():
-        if len(line)<27 or not line[16] in ['A',' '] or (
-            not line.startswith("ATOM  ") and not line.startswith("TER"
-            ) and not (line.startswith("HETATM") and line[17:20]=="MSE")):
-            continue
-        resSeq=line[22:27] # residue sequence number
-        current_chainID=line[21] # chain identifier
+## read aligned histone sequences from msa file
+def read_prot_sequences(prot_seq_file):
+    prot_seq = {}     # dictionary to store all histone sequences from  MSA
+    with open(prot_seq_file, 'r') as reader:  
+        for lines in (reader.read().split(">")[1:]):
+            uniprotID = lines.split("\n")[0].split("|")[1].lower()
+            aligned_seq = joint_list_to_string(lines.split("\n")[1:],"")
+            prot_seq[uniprotID] = aligned_seq
+    return prot_seq 
         
-        if not current_old_index: # first residue encountered
-            current_old_index=resSeq # residue number in origin PDB
-            current_new_index=find_next_amino_acid(sequence_ref,sequence_pdb)
-            chainID=current_chainID
-            resSeq_new=str(current_new_index)
-            resSeq_new=' '*(4-len(resSeq_new))+resSeq_new+' '
-        elif resSeq!=current_old_index:
-            current_new_index=find_next_amino_acid(sequence_ref,
-                sequence_pdb,current_new_index)
-            current_old_index=resSeq
-            resSeq_new=str(current_new_index)
-            resSeq_new=' '*(4-len(resSeq_new))+resSeq_new+' '
-
-        if current_new_index==0: # reach the end of alignment
-            break
-        elif current_new_index>0: # no gap in sequence_ref:
-            PDBtxt_reindex+=line[:16]+' '+line[17:22]+resSeq_new+line[27:]+'\n'
-    return PDBtxt_reindex
-
-def reindex_pdb(startindex,infile,clean=True):
-    '''parse PDB file "infile", reindex it according to start index or
-    sequence file "startindex", and return the text of renumbered PDB
-    '''
-    fp=open(infile,'rU')
-    PDBtxt=''
-    for line in fp.read().splitlines():
-        if line.startswith("END"):
-            if clean:
-                line=line.replace("ENDMDL","END   ")
-            PDBtxt+=line+'\n'
-            break
-        if line.startswith("ATOM  ") or line.startswith("TER") or (
-            clean==False and not line[:6] in ["DBREF ","SEQADV","MODRES",
-            "HELIX ","SHEET ","SSBOND","SITE  "]):
-            PDBtxt+=line+'\n'
-    fp.close()
-
-    if os.path.isfile(startindex):
-        from NWalign import readFastaOrRawSequence
-        sequence_ref=readFastaOrRawSequence(startindex).replace('-','')
-        PDBtxt_reindex=reindex_pdb_by_sequence(sequence_ref,PDBtxt)
-    else:
-        PDBtxt_reindex=reindex_pdb_by_index(startindex,PDBtxt)
-    return PDBtxt_reindex
-
-if __name__=="__main__":
-    #### parse commandline arguments ####
-    clean=True
+### mapping binding site in the aligned histone sequences and count binding partners per sites   
+def map_binding_sites_to_msa(binding_sites_file, prot_seq_file, output_resi_map_per_his,output_resi_map_consensus): 
+#    binding_sites_file = "../raw_binding_mutation/PDB_H2A_binding_sites.txt"
+#    prot_seq_file = "combined_H2A_msa.fa"
     
-    argv=[]
-    for arg in sys.argv[1:]:
-        if arg.startswith("-clean="):
-            clean=(arg[len("-clean="):].lower()=="true")
-        elif arg.startswith('-') and len(set(arg[1:])-set(map(str,range(10)))):
-            sys.stderr.write("ERROR! Unknown option %s\n"%arg)
-            exit()
-        else:
-            argv.append(arg)
+    ## read data files contain the binding sites sites informations 
+    df = pd.read_table(binding_sites_file, skiprows = 1, header=None,     \
+                          names = ["histone_uniprot_ID", "histone_type", "binding_site_index", \
+                                   "partner_uniprot_ID", "sources"]) 
+    #read aligned histone sequences from msa file
+    all_msa_seq = read_prot_sequences(prot_seq_file) 
+    
+    ## map old residue index to new index
+    new_binding_sites_msa = pd.DataFrame() # dataframe to save new binding sites indexes on aligned sequences
+    for i in range(0,len(df)):
+        his_uniprot_ID = df["histone_uniprot_ID"][i]
+        old_bindsite_index = df["binding_site_index"][i]
+        BP_uniprot_ID = df["partner_uniprot_ID"][i]
+        data_source = df["sources"][i]
+        
+        ## get the aligned seq and original seq for a histone
+        seq_aligned = all_msa_seq[his_uniprot_ID]
+        seq_original = seq_aligned.replace('-', '')
+        
+        ## find new index in aligned seq
+        for resdi in range(old_bindsite_index, len(seq_aligned) +1 ):
+            if(seq_original[0:old_bindsite_index] == seq_aligned[0:resdi].replace('-', '')):
+                new_bindsite_index = resdi
+                break
+         ## save new residue index to dataframe
+        if data_source == "PDB" :
+            new_binding_sites_msa = new_binding_sites_msa.append({"his_uniprot_ID": his_uniprot_ID, \
+            "new_bindsite_index": new_bindsite_index,"partner_uniprot_ID": BP_uniprot_ID}, ignore_index=True)
+                     
+        elif data_source == "XLMS":
+            new_binding_sites_msa = new_binding_sites_msa.append({"his_uniprot_ID": his_uniprot_ID, \
+            "new_bindsite_index": new_bindsite_index -1,"partner_uniprot_ID": BP_uniprot_ID}, ignore_index=True)
+            new_binding_sites_msa = new_binding_sites_msa.append({"his_uniprot_ID": his_uniprot_ID, \
+            "new_bindsite_index": new_bindsite_index,"partner_uniprot_ID": BP_uniprot_ID}, ignore_index=True)
+            new_binding_sites_msa = new_binding_sites_msa.append({"his_uniprot_ID": his_uniprot_ID, \
+            "new_bindsite_index": new_bindsite_index+1,"partner_uniprot_ID": BP_uniprot_ID}, ignore_index=True)
 
-    if len(argv)<2:
-        sys.stderr.write(docstring)
-        exit()
+    ## count number of binding partners per residue per histone variant
+    with open(output_resi_map_per_his, 'w') as myfile:
+        all_his_uniprotIDs= np.unique(new_binding_sites_msa["his_uniprot_ID"])
+        
+        for his_uniprotID in all_his_uniprotIDs:
+            his_seq_binding_site_map = [0]*len(seq_aligned)
+            his_seq_binding_site_map.insert(0,his_uniprotID)
+            
+            
+            all_bindind_site = new_binding_sites_msa[new_binding_sites_msa["his_uniprot_ID"] == his_uniprotID]["new_bindsite_index"]
+                   
+            bind_sites_index = np.unique(all_bindind_site,return_counts=True)[0]
+            bind_sites_BP_count = np.unique(all_bindind_site,return_counts=True)[1]
+            
+            for res_idex, bp_count  in zip(bind_sites_index, bind_sites_BP_count):
+                his_seq_binding_site_map[int(res_idex)] = int(bp_count)
+                
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            wr.writerow(his_seq_binding_site_map)
+            
+   ## count number of binding partners per residue on  consensus sequence    
+    with open(output_resi_map_consensus, 'w') as myfile:
 
-    #### parse PDB file ####
-    PDBtxt_reindex=reindex_pdb(argv[0],argv[1],clean)
-
-    #### write PDB file ####
-    if len(argv)>1:
-        fp=open(argv[2],'w')
-        fp.write(PDBtxt_reindex)
-        fp.close()
-    else:
-        sys.stdout.write(PDBtxt)
+        binding_sites_consensus = new_binding_sites_msa.drop("his_uniprot_ID",axis=1).drop_duplicates().reset_index()
+        
+        for resi in range(1,len(seq_aligned)+1):
+            bp_count = 0
+            for i in range(0,len(binding_sites_consensus)):
+                if (resi == binding_sites_consensus["new_bindsite_index"][i]):
+                    bp_count +=1
+            myfile.write(str(resi) + "," + str(bp_count) + '\n' )  
+        
+if __name__=="__main__":
+    uniprotID_to_fa_msa("../raw_binding_mutation/combined_uniprot_H1.txt", "combined_H1.fa")
+    uniprotID_to_fa_msa("../raw_binding_mutation/combined_uniprot_H2A.txt", "combined_H2A.fa")
+    uniprotID_to_fa_msa("../raw_binding_mutation/combined_uniprot_H2B.txt", "combined_H2B.fa")
+    uniprotID_to_fa_msa("../raw_binding_mutation/combined_uniprot_H3.txt", "combined_H3.fa")
+    uniprotID_to_fa_msa("../raw_binding_mutation/combined_uniprot_H4.txt", "combined_H4.fa")
+    
+    map_binding_sites_to_msa("../raw_binding_mutation/PDB_H1_binding_sites.txt","combined_H1_msa.fa", './test/PDB_H1_mapping_binding_sites.txt',"./test/H1_binding_sites_consens_counts_PDB_unique.txt")   
+    map_binding_sites_to_msa("../raw_binding_mutation/PDB_H2A_binding_sites.txt","combined_H2A_msa.fa", './test/PDB_H2A_mapping_binding_sites.txt',"./test/H2A_binding_sites_consens_counts_PDB_unique.txt")   
+    map_binding_sites_to_msa("../raw_binding_mutation/PDB_H2B_binding_sites.txt","combined_H2B_msa.fa", './test/PDB_H2B_mapping_binding_sites.txt',"./test/H2B_binding_sites_consens_counts_PDB_unique.txt")   
+    map_binding_sites_to_msa("../raw_binding_mutation/PDB_H3_binding_sites.txt","combined_H3_msa.fa", './test/PDB_H3_mapping_binding_sites.txt',"./test/H3_binding_sites_consens_counts_PDB_unique.txt")   
+    map_binding_sites_to_msa("../raw_binding_mutation/PDB_H4_binding_sites.txt","combined_H4_msa.fa", './test/PDB_H4_mapping_binding_sites.txt',"./test/H4_binding_sites_consens_counts_PDB_unique.txt")   
+    
